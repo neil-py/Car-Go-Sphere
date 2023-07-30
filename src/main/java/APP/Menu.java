4d5c88f0231a4d4c0d8918e1d3ab4859174b2626
Neil Patrick Villanueva <neilpatz16@gmail.com>
2023-07-30 21:31:41 +0800

    Renting process added

--- src/main/java/APP/Menu.java
+++ src/main/java/APP/Menu.java
@@ -34,6 +34,7 @@ public class Menu extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("CarGoSphere Main");
@@ -72,6 +73,16 @@ public class Menu extends javax.swing.JFrame {
             }
         });
 
+        jButton4.setBackground(new java.awt.Color(26, 34, 65));
+        jButton4.setFont(new java.awt.Font("Yu Gothic UI", 1, 18)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(255, 255, 255));
+        jButton4.setText("Create Rental");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -79,6 +90,7 @@ public class Menu extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(96, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -93,7 +105,9 @@ public class Menu extends javax.swing.JFrame {
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(30, 30, 30)
                 .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(65, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(16, 16, 16))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -104,7 +118,9 @@ public class Menu extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 26, Short.MAX_VALUE))
         );
 
         pack();
@@ -125,6 +141,11 @@ public class Menu extends javax.swing.JFrame {
         // TODO add your handling code here:
         new ManageCust().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        new RentCar().setVisible(true);
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -171,6 +192,7 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
--- src/main/java/LIB/BookRental.java
+++ src/main/java/LIB/BookRental.java
@@ -5,17 +5,108 @@
 package LIB;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author neil
  */
 public class BookRental implements Backend{
+    
+    private int custID;
+    private int carID;
+    private String rentDate;
+    private String returnDate;
+    
+    
+    public BookRental(int custID, int carID, String rentDate, String returnDate){
+        this.custID=custID;
+        this.carID=carID;
+        this.rentDate=rentDate;
+        this.returnDate=returnDate;
+        CalculateRentRate();
+    }
+    
+    public int calculateDays() {
+        try {
+            LocalDate startDate = LocalDate.parse(rentDate);
+            LocalDate endDate = LocalDate.parse(returnDate);
 
+            // Calculate the difference in days between startDate and endDate
+            long differenceInDays = ChronoUnit.DAYS.between(startDate, endDate);
+
+            // Convert the result to int and return
+            return (int) differenceInDays;
+        } catch (Exception e) {
+            System.out.println("Error Parsing");
+            return 0; // Return a default value or handle the exception as needed
+        }
+    }
+    
+    public double CalculateRentRate(){
+        int days = calculateDays();
+        double rate = 0; // Initialize rate to a default value in case of an issue.
+        double totalRate=0;
+
+        String query = "SELECT ratePerDay FROM CARS WHERE carID=?;";
+        try {
+            DatabaseConnection connect = DatabaseConnection.getInstance();
+            try (Connection conn = connect.getConnection()) {
+                PreparedStatement pstmt = conn.prepareStatement(query);
+                pstmt.setInt(1, carID);
+
+                ResultSet rs = pstmt.executeQuery();
+
+                if (rs.next()) {
+                    rate = rs.getDouble("ratePerDay");
+                } else {
+                    System.out.println("Error: No rate found for carID: " + carID);
+                }
+            }
+            totalRate = (double) days * rate;
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return totalRate;
+    }
+   
 
     @Override
     public void register() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        double totalRentCost = CalculateRentRate();
+        String query2 = "INSERT INTO RENTALS (custID, carID, rentalDate, returnDate, totalCost, status)" +
+                                                     " VALUES (?, ?, ?, ?, ?, ?);";
+        
+        try{
+            System.out.println(rentDate);
+            System.out.println(returnDate);
+            System.out.println(totalRentCost);
+            DatabaseConnection connect = DatabaseConnection.getInstance();
+            Connection conn = connect.getConnection();
+            PreparedStatement pstmt2 = conn.prepareStatement(query2);
+            pstmt2.setInt(1, custID);
+            pstmt2.setInt(2, carID);
+            pstmt2.setString(3, rentDate);
+            pstmt2.setString(4, returnDate);
+            pstmt2.setDouble(5, totalRentCost);
+            pstmt2.setString(6, "Currently Rented");
+            pstmt2.executeUpdate();
+
+            conn.close();
+        } catch (SQLException e){
+            System.out.println("Error" + e.getMessage());
+        }
+        
     }
-    
 }
